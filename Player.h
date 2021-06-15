f33b7d0990398e6a6db05519f2984e033c75dea8
chunhian349 <83117787+chunhian349@users.noreply.github.com>
2021-06-15 22:48:52 +0800

    Update Player.h

--- Player.h
+++ Player.h
@@ -1,5 +1,8 @@
 #ifndef PLAYER_H
 #define PLAYER_H
+#include <string>
+#include <iostream>
+using namespace std;
 
 class Player
 {
@@ -8,22 +11,27 @@ class Player
 		{
 			string value;
 			string color;
-			Card *next
-		}
+			Card *next;
+		};
+		
 		Card *first;
 		Player *next;
 		int num;
 		
 	public:
-		Player(){ first = nullptr; next = nullptr; num = 0	};
-		~Player(){ clearCard(); };
+		Player(){ first = nullptr; next = nullptr; num = 0;	};
+		~Player(){ clearCard(); }
+		void setPlayerNext(Player*);
+		Player* getPlayerNext();
 		void insertCard(string, string);
 		void showInfo() const;
 		void playCard(int, string &, string &);
 		bool checkCard(string, string);
 		bool checkCard(int, string, string);
+		bool checkLegal(string, string);
 		int getCardNum() const {	return num;	};
 		void clearCard();
+		friend void switchPlayer(Player *&);
 };
 
 #endif
