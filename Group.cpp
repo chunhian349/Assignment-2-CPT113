6e2ee5e060798d235ab37881c3deeb901d64e186
chunhian349 <83117787+chunhian349@users.noreply.github.com>
2021-06-15 13:16:50 +0800

    Update Group.cpp

--- Group.cpp
+++ Group.cpp
@@ -2,33 +2,60 @@
 #include <iostream>
 using namespace std;
 
+Group::Group()
+{
+	Player* player1 = new Player;  
+	Player* player2 = new Player;
+	 
+	player1->next = player2; 
+	player2->next = player1;  
+	
+	current = player1;
+}
+
 void Group::showCards()
 {
-	current.showInfo();
+	current->showInfo();
 }
 
 void Group::draw(string value, string color)
 {
-	current.insertCard(value, color);
+	current->insertCard(value, color);
 }
 
-void Group::play(int &index, string &value, string &color)
+void Group::play(int position, string &value, string &color)
 {
-	current.playCard(index, value, color);
-	current = current->next;
+	current->playCard(position, value, color);
 }
 
 bool Group::isPlayable(string value, string color)
 {
-	return(current.checkCard(value, color);
+	return current->checkCard(value, color);
+}
+
+bool Group::isPlayable(int position, string value, string color)
+{
+	return current->checkCard(position, value, color);
 }
 
 void Group::clearPlayer()
 {
-	current.clearCard();
+	Player *playerPtr = nullptr;
+	
+	while(current != nullptr)
+	{
+		playerPtr = current;
+		current = current->next;
+		delete playerPtr;
+	}
 }
 
 void Group::switchPlayer()
 {
 	current = current->next;
+}
+
+int Group::playerCardNum()
+{
+	return current->getCardNum();
 }
