91b9e7b45da3916ebc03261684c3a8e1843f07db
chunhian349 <83117787+chunhian349@users.noreply.github.com>
2021-06-15 13:15:12 +0800

    Update Player.h

--- Player.h
+++ Player.h
@@ -12,13 +12,18 @@ class Player
 		}
 		Card *first;
 		Player *next;
+		int num;
+		
 	public:
-		Player(){ first = nullptr; next = nullptr; };
+		Player(){ first = nullptr; next = nullptr; num = 0	};
 		~Player(){ clearCard(); };
 		void insertCard(string, string);
 		void showInfo() const;
-		void playCard(int &, string &, string &);
+		void playCard(int, string &, string &);
 		bool checkCard(string, string);
-		bool isEmpty() { return (first == nullptr); };
+		bool checkCard(int, string, string);
+		int getCardNum() const {	return num;	};
 		void clearCard();
 };
+
+#endif
