bc44ca88b8421102db244e397a8c11a0ed9eefc7
chunhian349 <83117787+chunhian349@users.noreply.github.com>
2021-06-16 22:28:30 +0800

    Update CardStack.h

--- CardStack.h
+++ CardStack.h
@@ -3,21 +3,24 @@
 #include <string>
 using namespace std;
 
+//Stack data structure to store card objects
 class CardStack
 {
 	private:
+		//Card object is the node of the stack
 		struct Card
 		{
-			string value;
-			string color;
-			Card *next;
+			//Store the value of the card (0-9/Skip/Reverse/Draw 2/Wild/Wild Draw 4)
+			string value; 
+			string color; //Store the color of the card
+			Card *next; //Points next node
 		};
-		Card *cardTop;
+		Card *cardTop; //Points the card at top of the stack
 		
 	public:
-		CardStack(){cardTop = nullptr;};
-		~CardStack();
-		void push(string, string);
+		CardStack(){cardTop = nullptr;}
+		~CardStack(); 
+		void push(string, string); 
 		void pop(string&, string&);
 		void peekTop(string&, string&);
 		bool isEmpty();
